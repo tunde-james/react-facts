9feb5e8ed1766e3019df2fe11ded3cec67e0140e
Tunde James <tundejames04@gmail.com>
2022-07-01 22:27:35 +0100

    Added the nav component with it styles

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import ReactIcon from '../images/react-icon-small.png'
+import styles from '../css/nav.module.css'
+
+export default function Nav() {
+  return (
+    <nav className={styles.nav}>
+      <div className={styles.container}>
+        <div className={styles.nav__left}>
+          <img src={ReactIcon} alt="React icon logo" className={styles.logo} />
+          <p className={styles.logo__text}>ReactFacts</p>
+        </div>
+
+        <div className={styles.nav__right}>
+          <p className={styles.title}>react course - Project 1</p>
+        </div>
+      </div>
+    </nav>
+  )
+}
